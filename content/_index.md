fa7ad2805e6d551fecf30e593cf33066088a5447
Nikolay Kolomyytsev <kolya.ya@gmail.com>
2021-06-22 11:00:51 +0200

    init commit

---
+++ content/_index.md
@@ -0,0 +1,6 @@
+---
+title: "plus360 — Home page"
+subtitle: "We are watching you"
+---
+
+Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos. Text & photos.
---
+++ themes/p360/assets/js/footer-script.js
@@ -0,0 +1,25 @@
+
+const twinTopMenu = () => {
+  const headerTopMenu = document.querySelectorAll('.header-top > a')
+  const [topPortfolio, topLogo, topBuro] = headerTopMenu
+  
+  const headerBottomMenu = document.querySelectorAll('.header-bottom > ul')
+  const [portfolioMenu, buroMenu] = headerBottomMenu
+
+  topPortfolio.addEventListener('click', () => {
+    portfolioMenu.classList.remove('header-bottom__portfoulo-menu--hidden')
+    buroMenu.classList.add('header-bottom__buro-menu--hidden')
+  })
+
+  topLogo.addEventListener('click', () => {
+    portfolioMenu.classList.add('header-bottom__portfoulo-menu--hidden')
+    buroMenu.classList.add('header-bottom__buro-menu--hidden')
+  })
+
+  topBuro.addEventListener('click', () => {
+    portfolioMenu.classList.add('header-bottom__portfoulo-menu--hidden')
+    buroMenu.classList.remove('header-bottom__buro-menu--hidden')
+  })
+}
+
+twinTopMenu()
